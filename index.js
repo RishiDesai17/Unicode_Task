de330e5c7d1d9ae0e1b1a27e87f0467cff4b48f5
RishiDesai17 <rishindesai@yahoo.com>
2019-08-23 05:05:51 +0530

    upload

--- index.js
+++ index.js
@@ -6,4 +6,35 @@ $(window).on("scroll", function() {
     $('nav').addClass('bg-transparent');
     $('nav').removeClass('bg-dark');
   }
-})
+});
+
+$(document).ready(function() {
+  setTimeout(function() {
+    PopUp();
+  }, 1500);
+});
+
+function PopUp() {
+  $("#popup-surrounding").fadeIn('slow');
+}
+
+function Close() {
+  $("#popup-surrounding").fadeOut('slow');
+}
+var final = new Date(2019, 7, 31, 23, 59, 59).getTime();
+setInterval(function() {
+  var present = new Date().getTime();
+  var remainingTime = final - present;
+  var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " +
+    minutes + "m " + seconds + "s ";
+  if (remainingTime < 0) {
+    document.getElementById("countdown").innerHTML = "";
+  }
+}, 1000);
+$("#title1").mouseover(function() {
+  $("#share").show();
+});
